a2c1c04c870e92c0371ad729de258156b629cfe0
Filippos Sakellaropoulos <phisakel@gmail.com>
2023-11-22 01:08:59 +0200

    Update package dependencies

--- Package.swift
+++ Package.swift
@@ -14,10 +14,9 @@ let package = Package(
     ],
     dependencies: [
 		.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-transfer.git", branch: "develop"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-storage.git", branch: "develop"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-transfer.git", .upToNextMinor(from: "0.1.0")),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-storage.git", .upToNextMinor(from: "0.1.0")),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-siop-openid4vp-swift.git", .upToNextMinor(from: "0.0.65")),
-		.package(url: "https://github.com/apple/swift-log.git", branch: "main"),
 	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -27,7 +26,6 @@ let package = Package(
 		    	.product(name: "MdocDataTransfer18013", package: "eudi-lib-ios-iso18013-data-transfer"),
 				.product(name: "WalletStorage", package: "eudi-lib-ios-wallet-storage"),
 				.product(name: "SiopOpenID4VP", package: "eudi-lib-ios-siop-openid4vp-swift"),
-	    	    .product(name: "Logging", package: "swift-log"),
 	        ]
         ),
         .testTarget(
