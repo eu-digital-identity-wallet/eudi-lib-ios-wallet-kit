689f22e008a685708413279efe5573978501aa3d
Filippos Sakellaropoulos <phisakel@gmail.com>
2024-02-02 10:48:23 +0200

    use X509CertificateChainVerifier

--- Sources/eudi-lib-ios-wallet-kit/Services/OpenId4VpService.swift
+++ Sources/eudi-lib-ios-wallet-kit/Services/OpenId4VpService.swift
@@ -142,13 +142,10 @@ public class OpenId4VpService: PresentationService {
 	}
 	
 	static var chainVerifier: CertificateTrust = { certificates in
-	/*	let chainVerifier = X509CertificateChainVerifier()
-		let verified = try? chainVerifier.verifyCertificateChain(
-			base64Certificates: certificates
-		)
-		return chainVerifier.isChainTrustResultSuccesful(verified ?? .failure)
-	 */
-		return true
+		let chainVerifier = X509CertificateChainVerifier()
+		let verified = try? chainVerifier.verifyCertificateChain(base64Certificates: certificates)
+		let result = chainVerifier.isChainTrustResultSuccesful(verified ?? .failure)
+		return result
 	}
 	
 	/// OpenId4VP wallet configuration
@@ -157,7 +154,7 @@ public class OpenId4VpService: PresentationService {
 			  let rsaPublicKey = try? KeyController.generateRSAPublicKey(from: rsaPrivateKey) else { return nil }
 		guard let rsaJWK = try? RSAPublicKey(publicKey: rsaPublicKey, additionalParameters: ["use": "sig", "kid": UUID().uuidString, "alg": "RS256"]) else { return nil }
 		guard let keySet = try? WebKeySet(jwk: rsaJWK) else { return nil }
-		let res = WalletOpenId4VPConfiguration(subjectSyntaxTypesSupported: [], preferredSubjectSyntaxType: .jwkThumbprint, decentralizedIdentifier: try! DecentralizedIdentifier(rawValue: "did:example:123"), idTokenTTL: 10 * 60, presentationDefinitionUriSupported: true, signingKey: privateKey, signingKeySet: keySet, supportedClientIdSchemes: [.x509SanDns(trust: chainVerifier)], vpFormatsSupported: [])
+		let res = WalletOpenId4VPConfiguration(subjectSyntaxTypesSupported: [], preferredSubjectSyntaxType: .jwkThumbprint, decentralizedIdentifier: try! DecentralizedIdentifier(rawValue: "did:example:123"), signingKey: privateKey, signingKeySet: keySet, supportedClientIdSchemes: [.x509SanDns(trust: chainVerifier)], vpFormatsSupported: [])
 		return res
 	}
 
