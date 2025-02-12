2de8622b1b6eff6a550f5a46acfcd173255eeaf0
Filippos Sakellaropoulos <phisakel@gmail.com>
2025-02-12 21:20:35 +0200

    Update dependencies and refactor OpenId4VCIConfig initialization in EudiWallet

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-storage.git", exact: "0.4.5"),
     .package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-sdjwt-swift.git", exact: "0.5.1"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-siop-openid4vp-swift.git", exact: "0.6.4"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-openid4vci-swift.git", exact: "0.10.1"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-openid4vci-swift.git", exact: "0.12.0"),
 	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
