17edb5a73e3a6cae1e8dd3092f0ce86b07a58780
Filippos Sakellaropoulos <phisakel@gmail.com>
2025-02-13 15:34:52 +0200

    Update eudi-lib-ios-iso18013-data-transfer dependency to version 0.5.5

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
     .package(url: "https://github.com/apple/swift-log.git", from: "1.5.3"),
 		.package(url: "https://github.com/crspybits/swift-log-file", from: "0.1.0"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-transfer.git",  exact: "0.5.4"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-transfer.git",  exact: "0.5.5"),
 		//.package(path: "../eudi-lib-ios-iso18013-data-transfer"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-storage.git", exact: "0.4.5"),
     .package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-sdjwt-swift.git", exact: "0.5.1"),
