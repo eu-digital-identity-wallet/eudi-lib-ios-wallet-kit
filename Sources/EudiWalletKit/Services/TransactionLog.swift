a0beb8f4dafd03e07eaa51ca62eb094997d7ed87
Filippos Sakellaropoulos <phisakel@gmail.com>
2025-04-08 00:59:02 +0300

    parseDocClaimsDecodables for CBOR, inner log types

--- Sources/EudiWalletKit/Services/TransactionLog.swift
+++ Sources/EudiWalletKit/Services/TransactionLog.swift
@@ -8,7 +8,7 @@ import Copyable
 /// Transaction log.
 @Copyable
 public struct TransactionLog: Sendable, Codable {
-	public init(timestamp: Int64, status: Status, errorMessage: String? = nil, rawRequest: Data? = nil, rawResponse: Data? = nil, relyingParty: RelyingParty? = nil, type: TransactionLogType, dataFormat: LogDataFormat, sessionTranscript: Data? = nil, docMetadata: [Data]? = nil) {
+	public init(timestamp: Int64, status: Status, errorMessage: String? = nil, rawRequest: Data? = nil, rawResponse: Data? = nil, relyingParty: RelyingParty? = nil, type: TransactionLog.LogType, dataFormat: TransactionLog.DataFormat, sessionTranscript: Data? = nil, docMetadata: [Data]? = nil) {
 		// Initialize the properties with the provided values
 		self.timestamp = timestamp
 		self.status = status
@@ -28,16 +28,57 @@ public struct TransactionLog: Sendable, Codable {
 	let rawRequest: Data?
 	let rawResponse: Data?
 	let relyingParty: RelyingParty?
-	let type: TransactionLogType
-	let dataFormat: LogDataFormat
+	let type: LogType
+	let dataFormat: DataFormat
 	let sessionTranscript: Data?
     let docMetadata: [Data]?
+
+	public enum DataFormat: Int, Sendable, Codable {
+		case cbor
+		case json
+	}
+
+	public struct RelyingParty: Codable, Sendable {
+		/// The name of the relying party
+		let name: String
+		/// Whether the relying party is verified.
+		let isVerified: Bool
+		/// The certificate chain of the relying party.
+		let certificateChain: [Data]
+		/// The reader authentication data. This is populated only when mdoc presentation is used.
+		let readerAuth: Data?
+	}
+
+	public enum LogType: Int, Sendable, Codable {
+		case presentation
+		case issuance
+		case signing
+	}
+
+	public enum Status: Int, Sendable, Codable {
+		/// Indicates that the transaction is incomplete
+		case incomplete
+		// Indicates that the transaction was completed successfully.
+		case completed
+		// Indicates that the transaction failed.
+		case failed
+	}
 }
 
-public enum LogDataFormat: Int, Sendable, Codable {
-	case cbor
-	case json
+struct PresentationTransactionLog {
+	let timestamp: Date
+	let status: TransactionLog.Status
+	let relyingParty: TransactionLog.RelyingParty
+	let documents: [DocClaimsDecodable]
+
+	public init(_ transactionLog: TransactionLog) async throws {
+		timestamp = Date(timeIntervalSince1970: TimeInterval(transactionLog.timestamp))
+		status = transactionLog.status
+		relyingParty = transactionLog.relyingParty ?? TransactionLog.RelyingParty(name: "", isVerified: false, certificateChain: [], readerAuth: nil)
+		documents = []
+	}
 }
+
 
 public struct VpRequestPayload: Codable {
 	let presentationDefinition: PresentationDefinition?
@@ -51,36 +92,11 @@ public struct VpResponsePayload: Codable {
 
 public struct VerifiablePresentationPayload: Codable {
 	let id: String
-	let type: LogDataFormat
+	let type: TransactionLog.DataFormat
 	let docData: String
 	let metadata: DocMetadata?
 }
 
-public struct RelyingParty: Codable, Sendable {
-	/// The name of the relying party
-	let name: String
-	/// Whether the relying party is verified.
-	let isVerified: Bool
-	/// The certificate chain of the relying party.
-	let certificateChain: [Data]
-	/// The reader authentication data. This is populated only when mdoc presentation is used.
-	let readerAuth: Data?
-}
-
-public enum TransactionLogType: Int, Sendable, Codable {
-	case presentation
-	case issuance
-	case signing
-}
-
-public enum Status: Int, Sendable, Codable {
-    /// Indicates that the transaction is incomplete
-    case incomplete
-    // Indicates that the transaction was completed successfully.
-    case completed
-    // Indicates that the transaction failed.
-    case failed
-}
 
 /// A logger for transactions.
 ///
--- Sources/EudiWalletKit/Services/TransactionLogUtils.swift
+++ Sources/EudiWalletKit/Services/TransactionLogUtils.swift
@@ -1,5 +1,8 @@
 import Foundation
+import MdocDataModel18013
 import MdocDataTransfer18013
+import WalletStorage
+import SwiftCBOR
 
 class TransactionLogUtils {
 	@MainActor
@@ -7,7 +10,7 @@ class TransactionLogUtils {
 
 	private init() {}
 
-	static func initializeTransactionLog(type: TransactionLogType, dataFormat: LogDataFormat) -> TransactionLog {
+	static func initializeTransactionLog(type: TransactionLog.LogType, dataFormat: TransactionLog.DataFormat) -> TransactionLog {
 		let transactionLog = TransactionLog(timestamp: Int64(Date.now.timeIntervalSince1970.rounded()), status: .incomplete, type: type, dataFormat: dataFormat)
 		return transactionLog
 	}
@@ -27,6 +30,7 @@ class TransactionLogUtils {
 		transactionLog = transactionLog.copy(timestamp: Int64(Date.now.timeIntervalSince1970.rounded()), status: .completed, rawResponse: bleServerTransfer.deviceResponseBytes, sessionTranscript: sessionTranscript)
 	}
 
+/*
 	func getTransactionLogFilePath() -> String {
 		let fileManager = FileManager.default
 		let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first!
@@ -43,9 +47,31 @@ class TransactionLogUtils {
 			print("Error saving transaction log: \(error)")
 		}
 	}
+	*/
 
-	static func getRelyingParty(_ requestInfo: UserRequestInfo) -> RelyingParty? {
+	static func getRelyingParty(_ requestInfo: UserRequestInfo) -> TransactionLog.RelyingParty? {
 		guard let name = requestInfo.readerCertificateIssuer else { return nil }
-		return RelyingParty(name: name, isVerified: requestInfo.readerAuthValidated ?? false, certificateChain: requestInfo.certificateChain ?? [], readerAuth: requestInfo.readerAuthBytes)
+		return TransactionLog.RelyingParty(name: name, isVerified: requestInfo.readerAuthValidated ?? false, certificateChain: requestInfo.certificateChain ?? [], readerAuth: requestInfo.readerAuthBytes)
+	}
+
+	static func parseDocClaimsDecodables(_ transactionLog: TransactionLog, uiCulture: String?) -> [any DocClaimsDecodable] {
+		guard let raw = transactionLog.rawResponse else { return [] }
+		var res = [any DocClaimsDecodable]()
+		let docMetadatas = transactionLog.docMetadata?.compactMap { DocMetadata(from: Data?($0)) } ?? []
+		if transactionLog.dataFormat == .cbor {
+			guard let dr = DeviceResponse(data: raw.bytes) else { return [] }
+			for doc in dr.documents ?? [] {
+				let docMetadata = docMetadatas.first(where: { $0.docType == doc.docType })
+				if let docDecodable = parseCBORDocClaimsDecodable(id: docMetadata?.docId ?? UUID().uuidString, docType: doc.docType, issuerSigned: doc.issuerSigned, metadata: docMetadata?.toData(), uiCulture: uiCulture) {
+					res.append(docDecodable)
+				}
+			}
+		}
+		return res
+	}
+
+	static func parseCBORDocClaimsDecodable(id: String, docType: String, issuerSigned: IssuerSigned, metadata: Data?, uiCulture: String?) -> (any DocClaimsDecodable)? {
+		let document = WalletStorage.Document(id: id, docType: docType, docDataFormat: .cbor, data: Data(issuerSigned.encode(options: CBOROptions())), secureAreaName: SecureAreaRegistry.DeviceSecureArea.software.rawValue, createdAt: .now, modifiedAt: .now, metadata: metadata, displayName: docType, status: .issued)
+		return StorageManager.toClaimsModel(doc: document, uiCulture: uiCulture)
 	}
 }
