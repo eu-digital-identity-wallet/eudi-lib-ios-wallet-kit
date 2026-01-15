f786e92cb09ec4e523b98328b84b3a032e2cb76e
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-01-15 02:15:14 +0200

    temp: niscy data transfer

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
 		.package(url: "https://github.com/crspybits/swift-log-file", from: "0.1.0"),
-		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-iso18013-data-transfer.git", exact: "0.8.5"),
+        .package(url: "https://github.com/niscy-eudiw/eudi-lib-ios-iso18013-data-model.git", branch: "main"),
+		.package(url: "https://github.com/niscy-eudiw/eudi-lib-ios-iso18013-data-transfer.git", branch: "main"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-storage.git", exact: "0.8.0"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-sdjwt-swift.git", exact: "0.10.1"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-openid4vp-swift.git", exact: "0.19.0"),
@@ -29,7 +30,8 @@ let package = Package(
 		.target(
 			name: "EudiWalletKit",
 			dependencies: [
-				.product(name: "MdocDataTransfer18013", package: "eudi-lib-ios-iso18013-data-transfer"),
+		                .product(name: "MdocDataModel18013", package: "eudi-lib-ios-iso18013-data-model"),
+		.product(name: "MdocDataTransfer18013", package: "eudi-lib-ios-iso18013-data-transfer"),
 				.product(name: "WalletStorage", package: "eudi-lib-ios-wallet-storage"),
 				.product(name: "OpenID4VP", package: "eudi-lib-ios-openid4vp-swift"),
 				.product(name: "OpenID4VCI", package: "eudi-lib-ios-openid4vci-swift"),
