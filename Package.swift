eb69c08609203f3fda4dff2488c98935c64607ba
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-02-05 17:11:46 +0200

    Update OpenID4VCI dependency to the latest version

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-wallet-storage.git", exact: "0.8.0"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-sdjwt-swift.git", exact: "0.13.0"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-openid4vp-swift.git", exact: "0.20.0"),
-		.package(url: "https://github.com/niscy-eudiw/eudi-lib-ios-openid4vci-swift.git", exact: "0.8.0"),
+		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-openid4vci-swift.git", exact: "0.20.0"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/eudi-lib-ios-statium-swift.git", exact: "0.3.1"),
 		.package(url: "https://github.com/eu-digital-identity-wallet/SwiftCopyableMacro.git", from: "0.0.3")
 	],
