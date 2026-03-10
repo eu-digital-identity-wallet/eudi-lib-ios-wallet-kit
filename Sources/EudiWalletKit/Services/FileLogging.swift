d1235242201b9649cf4b30850c11929e4b513ec1
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-03-11 01:59:49 +0200

    Refactor FileLogging to use FileHandlerOutputStream for improved type safety

--- Sources/EudiWalletKit/Services/FileLogging.swift
+++ Sources/EudiWalletKit/Services/FileLogging.swift
@@ -1,8 +1,24 @@
+/*
+ * Copyright (c) 2023 European Commission
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Code taken from package "https://github.com/crspybits/swift-log-file"
 import Logging
 import Foundation
-
 // Adapted from https://nshipster.com/textoutputstream/
-struct FileHandlerOutputStream: TextOutputStream {
+struct FileHandlerOutputStream: TextOutputStream, Sendable {
     enum FileHandlerOutputStream: Error {
         case couldNotCreateFile
     }
@@ -31,7 +47,7 @@ struct FileHandlerOutputStream: TextOutputStream {
 }
 
 public struct FileLogging {
-    let stream: TextOutputStream
+    let stream: FileHandlerOutputStream
     private var localFile: URL
     
     public init(to localFile: URL) throws {
@@ -54,7 +70,7 @@ public struct FileLogging {
 /// `FileLogHandler` is a simple implementation of `LogHandler` for directing
 /// `Logger` output to a local file. Appends log output to this file, even across constructor calls.
 public struct FileLogHandler: LogHandler {
-    private let stream: TextOutputStream
+    private let stream: FileHandlerOutputStream
     private var label: String
     
     public var logLevel: Logger.Level = .info
