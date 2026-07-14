f53a8c04fa62838cba0013c5c4a4b4c7433ce071
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-07-14 14:35:37 +0300

    Show a friendly error in case status check failure

--- Sources/EudiWalletKit/Models/WalletError.swift
+++ Sources/EudiWalletKit/Models/WalletError.swift
@@ -43,6 +43,8 @@ public struct WalletError: LocalizedError {
 		case trustError
 		/// invalid status token
 		case invalidStatusToken
+		/// status check failed
+		case statusCheckFailed
 		/// Credential offer could not be resolved
 		case offerResolutionFailed
 		/// Issuer metadata could not be resolved
