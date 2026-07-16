9ceb879bd12111790dc566d82dd8fd7cbedf9c84
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-07-16 03:13:50 +0300

    Refactor getSdJwtPresentation to change dcql query extraction. Fixes array query

--- Sources/EudiWalletKit/Services/Openid4VpUtils.swift
+++ Sources/EudiWalletKit/Services/Openid4VpUtils.swift
@@ -230,8 +230,9 @@ class OpenId4VpUtils {
 	static func getSdJwtPresentation(_ sdJwt: SignedSDJWT, hashingAlg: HashingAlgorithm, signer: SecureAreaSigner, signAlg: JSONWebAlgorithms.SigningAlgorithm, requestItems: [RequestItem], nonce: String, aud: String, transactionData: [TransactionData]?) async throws -> SignedSDJWT? {
 		guard let allPathsDict = (try sdJwt.recreateClaims()).disclosuresPerClaimPath else { throw WalletError(description: "No disclosures found", code: .internalError) }
 		let allPaths = Array(allPathsDict.keys)
-		let query = Set(allPaths.filter { path in requestItems.contains(where: { r in r.claimPath == path }) })
-		for q in query { print(q.value.map(\.description) ) }
+		print(allPaths.map { p in p.value.map { $0.description } })
+		let query = Set(requestItems.map(\.claimPath)) // allPaths.filter { path in requestItems.contains(where: { r in r.claimPath == path }) })
+		logger.info("Dcql query: \(query.map { $0.value.map(\.description) })")
 		let presentedSdJwt = try sdJwt.present(query: query)
 		guard let presentedSdJwt else { return nil }
 		let digestCreator = DigestCreator(hashingAlgorithm: hashingAlg)
