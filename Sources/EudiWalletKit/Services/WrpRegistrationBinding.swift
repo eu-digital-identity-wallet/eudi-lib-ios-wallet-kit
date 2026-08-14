9d724fe0972d4363d43d2c6a1579b816a260f3c6
Filippos Sakellaropoulos <phisakel@gmail.com>
2026-08-14 14:58:20 +0300

    fix: update intermediary property to support multiple entries in WrpRegistrationPolicy

--- Sources/EudiWalletKit/Services/WrpRegistrationBinding.swift
+++ Sources/EudiWalletKit/Services/WrpRegistrationBinding.swift
@@ -28,7 +28,7 @@ extension WrpRegistrationPolicy {
             return false
         }
         if let intermediary {
-            return intermediary.identifier == presenterId
+			return intermediary.compactMap(\.identifier).contains(presenterId)
         }
         return identifiers.contains { $0.value == presenterId }
     }
